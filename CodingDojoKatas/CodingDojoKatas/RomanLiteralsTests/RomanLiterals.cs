25bac4bcd4ea624c34e4679d062d324dc22010ad
Hinrich Aue <hinrich.aue@cytena.com>
2021-08-16 12:00:54 +0200

    finished roman literals dojo

---
+++ CodingDojoKatas/CodingDojoKatas/RomanLiteralsTests/RomanLiterals.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using RomanLiterals;
+using System;
+using System.Collections.Generic;
+
+namespace RomanLiteralsTests
+{
+    [TestFixture]
+    public class RomanLiteralsParserTests
+    {
+        [TestCase("I", ExpectedResult = 1)]
+        [TestCase("II", ExpectedResult = 2)]
+        [TestCase("VII", ExpectedResult = 7)]
+        [TestCase("IIV", ExpectedResult = 3)]
+        [TestCase("IV", ExpectedResult = 4)]
+        [TestCase("V", ExpectedResult = 5)]
+        [TestCase("X", ExpectedResult = 10)]
+        [TestCase("C", ExpectedResult = 100)]
+        [TestCase("M", ExpectedResult = 1000)]
+        [TestCase("L", ExpectedResult = 50)]
+        [TestCase("D", ExpectedResult = 500)]
+        [TestCase("ID", ExpectedResult = 499)]
+        [TestCase("CDL", ExpectedResult = 450)]
+        [TestCase("IX", ExpectedResult = 9)]
+        [TestCase("XL", ExpectedResult = 40)]
+        [TestCase("XC", ExpectedResult = 90)]
+        [TestCase("CD", ExpectedResult = 400)]
+        [TestCase("CM", ExpectedResult = 900)]
+        public int ParseRomanLiteral_Digits_ParsedCorrectly(string romanLiteral)
+        {
+            var target = new RomanLiteralsParser();
+
+            return target.ParseRomanLiteral(romanLiteral);
+        }
+
+        [TestCase("a")]
+        public void ParseRomanLiteral_Digits_ParsedUnknownCharacter(string romanLiteral)
+        {
+            var target = new RomanLiteralsParser();
+
+            Assert.That(() => target.ParseRomanLiteral(romanLiteral), Throws.Exception.TypeOf<KeyNotFoundException>());
+        }
+    }
+}
---
+++ CodingDojoKatas/CodingDojoKatas/RomanLiteras/RomanLiterasParser.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace RomanLiterals
+{
+
+    public class RomanLiteralsParser
+    {
+        private int _currentValue;
+        private int _biggestDigit;
+
+        private static readonly Dictionary<char, int> ValueMap = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 },
+        };
+
+        public int ParseRomanLiteral(string romanLiteral)
+        {
+            if (romanLiteral.Length == 1)
+            {
+                int currentDigitValue = ValueMap[romanLiteral[0]];
+
+                AccumulateCurrentLiteralValue(currentDigitValue);
+            }
+            else if (romanLiteral.Length > 1)
+            {
+                var rightmostDigitIndex = romanLiteral.Length - 1;
+
+                int currentDigitValue = ValueMap[romanLiteral[rightmostDigitIndex]];
+
+                AccumulateCurrentLiteralValue(currentDigitValue);
+
+                ParseRomanLiteral(romanLiteral.Substring(0, romanLiteral.Length - 1));
+            }
+
+            return _currentValue;
+        }
+
+        private void AccumulateCurrentLiteralValue(int currentDigitValue)
+        {
+            if (currentDigitValue >= _biggestDigit)
+            {
+                _biggestDigit = currentDigitValue;
+                _currentValue += currentDigitValue;
+            }
+            else
+            {
+                _currentValue -= currentDigitValue;
+            }
+        }
+    }
+}
