91adddee9f849b0582effd415db9f6f953f385c4
Hinrich Aue <hinrich.aue@cytena.com>
2021-11-22 12:09:31 +0100

    Add ViewModel and tests for it

--- CodingDojoKatas/BowlingAppModel/Throw.cs
+++ CodingDojoKatas/BowlingAppModel/Throw.cs
@@ -2,6 +2,17 @@
 {
     public enum Throw
     {
-        None = 0,
+        Miss = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Strike = 10,
+        Spare = 11,
     }
 }
